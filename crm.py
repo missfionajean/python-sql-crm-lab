4e3cbf3d1c4ce7809a2049341d7df2ff06218a03
missfionajean <missfionajean@gmail.com>
2025-01-07 16:09:33 -0500

    read and update paths finished for company table

--- crm.py
+++ crm.py
@@ -11,10 +11,7 @@ program_exit = False
 
 # navigation functions
 
-
-
-# main menu
-while program_exit == False:
+def main_menu():
     print("""MAIN MENU
 
 Please select from the following options:
@@ -23,7 +20,79 @@ Please select from the following options:
 2. Employees
 3. Exit
 """)
+
+
+def company_submenu():
+    print("""COMPANIES
+
+Please select from the following options:
+    
+1. Show All Companies
+2. Add New Company
+3. Update Company
+4. Delete Company
+5. Back to Main Menu
+""")
+    while True:
+        selection = input('> ')
+        if selection == '1':
+            cursor.execute('SELECT * FROM companies')
+            print(cursor.fetchall())
+        elif selection == '2':
+            company_name = input('Enter company name: ')
+            cursor.execute('INSERT INTO companies (name) VALUES (%s)', [company_name])
+            connection.commit()
+            print('Company successfully added to database!')
+        elif selection == '3':
+            # stores entire company table in list
+            cursor.execute('SELECT * FROM companies')
+            all_companies = cursor.fetchall()
+            # for loop to display list in readable format
+            print("All Companies:")
+            for company in all_companies:
+                print(str(company[0]) + '.' + ' ' + company[1])
+            # asks user for required update info
+            company_id = input('Enter the # of the company to be updated: ')
+            company_name = input('Enter new company name: ')
+            # commits update to sql database in back end
+            cursor.execute('UPDATE companies SET name = %s WHERE id = %s', [company_name, company_id])
+            connection.commit()
+            # prints success message for user
+            print('Company name successfully updated!')
+        # elif selection == '4':
+
+        elif selection == '5':
+            main_menu()
+
+
+def employee_submenu():
+    print("""EMPLOYEES
+
+Please select from the following options:
+    
+1. Show All Employees
+2. Add New Employee
+3. Update Employee
+4. Delete Employee
+5. Back to Main Menu
+""")
+    while True:
+        selection = input('> ')
+        if selection == '1':
+            cursor.execute('SELECT * FROM employees')
+            print(cursor.fetchall())
+
+
+# main program loop
+main_menu()
+while program_exit == False:
     selection = input('> ')
+    if selection == '1':
+        company_submenu()
+    elif selection == '2':
+        employee_submenu()
+    elif selection == '3':
+        program_exit = True
 
 
 # grabs and displays all companies from table
